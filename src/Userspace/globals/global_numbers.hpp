14649f77b293866639c604d6229e7f6a6839b5ae
Viktor Kartashov <viktor.kartashov@cybereason.com>
2026-04-28 09:17:55 +0000

    fix: occasional plaintext in stderr when flatbuffers (binary) expected

--- src/Userspace/globals/global_numbers.hpp
+++ src/Userspace/globals/global_numbers.hpp
@@ -5,5 +5,6 @@ namespace owlsm::globals
     constexpr int KB = 1000;
     constexpr int MB = 1000 * KB;
     constexpr int MAX_LOG_FILES = 5;
+    constexpr unsigned int LIBBPF_LOG_MESSAGE_SIZE = 1024U;
     constexpr unsigned long long NANOSECONDS_IN_SECOND = 1000000000ULL;
 }
